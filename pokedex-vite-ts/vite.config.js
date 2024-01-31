12691a91d10569cd65ac598b2d6b80eca4797d77
Laurynas Bičkauskas <52818675+bickauskaslaurynas@users.noreply.github.com>
2024-01-31 11:22:51 +0200

    bump dependancy versions (#2)
    Co-authored-by: Laurynas Bičkauskas <laurynas@zyro.com>

--- pokedex-vite-ts/vite.config.js
+++ pokedex-vite-ts/vite.config.js
@@ -1,9 +1,8 @@
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
-/**
- * https://vitejs.dev/config/
- * @type {import('vite').UserConfig}
- */
-export default {
-  plugins: [vue()]
-}
+export default defineConfig({
+    plugins: [vue()]
+
+})
+
